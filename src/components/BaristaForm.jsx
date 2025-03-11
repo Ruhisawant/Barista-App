fc97104f6e9435330403f7bdf7c2b50043a605ac
Ruhisawant <ruhi.p.sawant@gmail.com>
2025-03-10 22:04:22 -0400

    Implemented JSON drinks

--- src/components/BaristaForm.jsx
+++ src/components/BaristaForm.jsx
@@ -1,7 +1,11 @@
 import React, {Component, useState} from 'react'
 import RecipeChoices from '../components/RecipeChoices'
+import drinksJson from '../drinks.json'
 
 const BaristaForm = () => {
+  const [currentDrink, setCurrentDrink] = useState('')
+  const [trueRecipe, setTrueRecipe] = useState({})
+
   const [inputs, setInputs] = useState({
     'temperature': '',
     'milk': '',
@@ -15,16 +19,32 @@ const BaristaForm = () => {
     'blended': ['yes', 'turbo', 'no']
   }
 
-  const onCheckAnswer = () => {
-      
+  const getNextDrink = () => {
+    let randomDrinkIndex = Math.floor(Math.random() * drinksJson.drinks.length)
+    setCurrentDrink(drinksJson.drinks[randomDrinkIndex].name)
+    setTrueRecipe(drinksJson.drinks[randomDrinkIndex].ingredients)
   }
+
   const onNewDrink = () => {
-
+    setInputs({
+      'temperature': '',
+      'milk': '',
+      'syrup': '',
+      'blended': '' })
+      
+    getNextDrink()
   }
 
+  const onCheckAnswer = () => {
+    
+  }
   return (
     <div>
       <h2>Hi, I'd like to order a:</h2>
+      <div className='drink-container'>
+        <h2 className='mini-header'>{currentDrink}</h2>
+        <button type='new-drink-button' className='button newDrink' onClick={onNewDrink}>🔄</button>
+      </div>
       <form>
         <h3>Temperature</h3>
         <div className='answer-space' >
